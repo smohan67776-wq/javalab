957800f836171ea6e7a4139a3b20463c66691cc1
smohan67776-wq <smohan67776@gmail.com>
2026-03-05 11:27:14 +0530

    Add files via upload

---
+++ program4/shapes/ShapeDemo.java
@@ -0,0 +1,25 @@
+package app;
+
+import shapes.*;
+
+public class ShapeDemo {
+
+    public static void main(String[] args) {
+
+        Shape s;
+
+        // Dynamic polymorphism
+        s = new Cube(3);
+        System.out.println("Cube Area = " + s.area());
+        System.out.println("Cube Volume = " + s.volume());
+
+        s = new Sphere(2);
+        System.out.println("Sphere Area = " + s.area());
+        System.out.println("Sphere Volume = " + s.volume());
+
+        // Static polymorphism
+        System.out.println("Square Area (static double) = " + Square.area(5.0));
+        System.out.println("Square Area (static int) = " + Square.area(6));
+        System.out.println("Circle Area (static) = " + Circle.area(4));
+    }
+}
